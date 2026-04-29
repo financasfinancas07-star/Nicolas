1be501f44040b53a07afa78b6d023212a89a5dc1
financasfinancas07-star <financasfinancas07@gmail.com>
2026-04-28 22:13:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,2 @@
-import streamlit as st
-import streamlit.components.v1 as components
-
-st.set_page_config(page_title="Gestão Financeira Familiar", layout="wide")
-
-# Lendo o seu arquivo HTML 
-with open("app_financeiro_completo.html", "r", encoding="utf-8") as f:
-    html_content = f.read()
-
-# Exibindo o HTML na tela - Aumentei a altura para 1200
-components.html(html_content, height=1200, scrolling=True)
+# Exemplo de como o Python deve salvar na planilha
+conn.create(data=df_novo_lancamento)
